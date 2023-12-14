c955688e0adba9a9f37f8d7ba47bfd7a5ae69c0b
F4de10 <149432824+F4de10@users.noreply.github.com>
2023-12-14 11:02:25 +0100

    Exercise 8

--- Python/exercise_8.py
+++ Python/exercise_8.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 months = []
 co2_values = []
@@ -9,41 +11,38 @@ with open("CO2.txt", "r") as file:
         months.append(int(line.split()[0]))
         co2_values.append(float(line.split()[1]))  # Convert CO2 values to float
 
-# Compute simple moving average over a 12-month interval
-moving_average = []
-for i in range(len(co2_values)):
-    if i < 12:
-        moving_average.append(sum(co2_values[: i + 1]) / (i + 1))
-    else:
-        moving_average.append(sum(co2_values[i - 11 : i + 1]) / 12)
+# Compute simple moving average over a 12-month interval (M = 12)
+M = 12
+moving_average = pd.DataFrame(co2_values).rolling(window=12).mean()
 
-# Compute centered moving average over a 12-month interval
-centered_moving_average = []
-for i in range(len(co2_values)):
-    if i < 6:
-        centered_moving_average.append(sum(co2_values[: i + 7]) / (i + 7))
-    elif i >= len(co2_values) - 6:
-        centered_moving_average.append(
-            sum(co2_values[i - 6 :]) / (len(co2_values) - i + 6)
-        )
-    else:
-        centered_moving_average.append(sum(co2_values[i - 6 : i + 7]) / 13)
+# Compute centered moving average over a 12-month interval (M = 12)
+M = 12
+centered_moving_average = (
+    pd.DataFrame(co2_values).rolling(window=12, center=True).mean()
+)
+diff = []
+for i, value in enumerate(centered_moving_average):
+    diff.extend(centered_moving_average[i] - moving_average[i])
 
 # Compute exponential moving average with alpha = 0.3
-exp_moving_average_03 = []
-exp_moving_average_07 = []
+exp_moving_average_03 = pd.DataFrame(co2_values).ewm(alpha=0.3).mean()
+exp_moving_average_07 = pd.DataFrame(co2_values).ewm(alpha=0.7).mean()
 
-for i in range(len(co2_values)):
-    if i == 0:
-        exp_moving_average_03.append(co2_values[i])
-        exp_moving_average_07.append(co2_values[i])
-    else:
-        exp_moving_average_03.append(
-            0.3 * co2_values[i] + 0.7 * exp_moving_average_03[i - 1]
-        )
-        exp_moving_average_07.append(
-            0.7 * co2_values[i] + 0.3 * exp_moving_average_07[i - 1]
-        )
+# Compute exponential moving average with alpha = 0.3
+# exp_moving_average_03 = []
+# exp_moving_average_07 = []
+
+# for i in range(len(co2_values)):
+#     if i == 0:
+#         exp_moving_average_03.append(co2_values[i])
+#         exp_moving_average_07.append(co2_values[i])
+#     else:
+#         exp_moving_average_03.append(
+#             0.3 * co2_values[i] + 0.7 * exp_moving_average_03[i - 1]
+#         )
+#         exp_moving_average_07.append(
+#             0.7 * co2_values[i] + 0.3 * exp_moving_average_07[i - 1]
+#         )
 
 # Compute autocorrelation coefficients
 autocorrelation_coefficients = []
@@ -95,8 +94,20 @@ plt.ylabel("Exponential Moving Average")
 plt.title("Exponential Moving Averages")
 plt.legend()
 
-# Plot autocorrelation coefficients
+# Combined plot
 plt.subplot(2, 3, 5)
+plt.plot(months, co2_values, label="Original CO2 Values")
+plt.plot(months, moving_average, label="Moving Average")
+plt.plot(months, centered_moving_average, label="Centered Moving Average")
+plt.plot(months, exp_moving_average_03, label="Alpha = 0.3")
+plt.plot(months, exp_moving_average_07, label="Alpha = 0.7")
+plt.xlabel("Months")
+plt.ylabel("CO2 Value")
+plt.title("Combined plot")
+plt.legend()
+
+# Plot autocorrelation coefficients
+plt.subplot(2, 3, 6)
 plt.stem(range(len(autocorrelation_coefficients)), autocorrelation_coefficients)
 plt.xlabel("Lag")
 plt.ylabel("Autocorrelation Coefficient")
